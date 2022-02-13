e066f0c25a13f736192c686a625605d7257c6cdf
xialiang <294046419@qq.com>
2022-02-13 17:52:45 +0800

    second commit

--- ydles_gateway/ydles_gateway_web/src/main/java/com/ydles/system/filters/UrlFilter.java
+++ ydles_gateway/ydles_gateway_web/src/main/java/com/ydles/system/filters/UrlFilter.java
@@ -3,9 +3,14 @@ package com.ydles.system.filters;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
+
+import java.net.InetSocketAddress;
 
 /**
  * @author xialiang
@@ -15,8 +20,14 @@ import reactor.core.publisher.Mono;
 public class UrlFilter implements GlobalFilter, Ordered {
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        return null;
-    }
+        ServerHttpRequest request = exchange.getRequest();
+        ServerHttpResponse response = exchange.getResponse();
+        InetSocketAddress remoteAddress = request.getRemoteAddress();
+        String hostName = remoteAddress.getHostName();
+        response.setStatusCode(HttpStatus.FORBIDDEN);
+
+        chain.filter(exchange);
+        return response.setComplete();    }
 
     @Override
     public int getOrder() {
